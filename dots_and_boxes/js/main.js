1a50b4f61cfd01a11210ad0ccc195f50fcb73049
Jacob Allen <jedijake@me.com>
2016-05-14 22:25:57 -0700

    add detail to board model

--- dots_and_boxes/js/main.js
+++ dots_and_boxes/js/main.js
@@ -5,3 +5,12 @@ console.log("linked");
 // 1. Each player takes turns clicking between dots which connects them.
 // 2. Once a player has completed a square (even if they did not make all borders of that square), the square belongs to that player.
 // 3. When the board is filled, the player with the most squares wins.
+
+/* Game Board */
+// I think the board will need to be an array of objects.
+// This will allow for the top, right, bottom, and left of each square to have a unique value.
+// When the T,R,B, and L values add to a certain amount, the square is filled and the previous player gets the point
+
+var $board = $("board")
+
+
