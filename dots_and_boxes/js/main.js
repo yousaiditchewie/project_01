8a4cfd2264e372ec8d1aa670c12a06c5a502c109
Jacob Allen <jedijake@me.com>
2016-05-14 21:44:22 -0700

    define the rules of the game in main.js

--- dots_and_boxes/js/main.js
+++ dots_and_boxes/js/main.js
@@ -2,3 +2,6 @@ console.log("linked");
 
 /* MOODEL */
 
+// 1. Each player takes turns clicking between dots which connects them.
+// 2. Once a player has completed a square (even if they did not make all borders of that square), the square belongs to that player.
+// 3. When the board is filled, the player with the most squares wins.
