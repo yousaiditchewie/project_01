50c7f1c352ffbe40410092ee5cbf3351516c142d
Jacob Allen <jedijake@me.com>
2016-05-16 15:34:20 -0700

    add click to play function, add player score variables

--- dots_and_boxes/js/main.js
+++ dots_and_boxes/js/main.js
@@ -4,6 +4,8 @@ console.log("linked");
 
 var player = 1; // value assigned to each square/border coordinate
 
+var player1Score = 0
+var player2Score = 0
 
 var board = [
             [0,0,0,0], // box 0
@@ -24,8 +26,11 @@ var board = [
             [0,0,0,0]  // box 15
             ];
 
+/* Compensate for the fact that some borders overlap by ensuring the values match for common borders
+
 
 /* BEHAVIOR */
+
 function play(y, x) {
   board[y][x] = player;
   if (player === 1) {
@@ -33,6 +38,23 @@ function play(y, x) {
   } else {
     player = 1;
   }
-  console.log(board, player);
+  console.log(player);
   return board;
 }
+
+// CLICK TO PLAY
+$('.square').on('click', function(e) {
+  if (e.offsetX < 0) {
+    console.log(parseInt(this.id.substring(3)), 3);
+    play(parseInt(this.id.substring(3)), 3);
+  } else if (e.offsetX >= e.target.clientWidth) {
+    console.log(parseInt(this.id.substring(3)), 1);
+    play(parseInt(this.id.substring(3)), 1);
+  } else if (e.offsetY < 0) {
+    console.log(parseInt(this.id.substring(3)), 0);
+    play(parseInt(this.id.substring(3)), 0);
+  } else if (e.offsetY >= e.target.clientHeight) {
+    console.log(parseInt(this.id.substring(3)), 2);
+    play(parseInt(this.id.substring(3)), 2);
+  }
+});
