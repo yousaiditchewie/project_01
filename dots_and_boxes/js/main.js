ea6d91c7471bbacd6eed5e4318096190f399b0be
Jacob Allen <jedijake@me.com>
2016-05-17 12:18:05 -0700

    correct makeEquals logic and refine 'taken' function

--- dots_and_boxes/js/main.js
+++ dots_and_boxes/js/main.js
@@ -29,37 +29,66 @@ var board = [
 /* make values match for overlapping borders */
 
 function makeEqual() {
+  for(var i = 0; i < 15; i++) {
+    // right logic
+    if(i !== 3 || i !== 7 || i !== 11) {
+      if (board[i][1] !== 0 && board[i + 1][3] === 0) {
+        board[i+1][3] = board[i][1];
+      } else {
+        board[i][1] = board[i+1][3];
+      }
+    }
+
+    // bottom logic
+    if (i < 12) {
+      if (board[i+4][0] !== 0 && board[i][2] === 0) {
+        board[i][2] = board[i+4][0];
+      } else {
+        board[i+4][0] = board[i][2];
+      }
+    }
+  }
   // right border for row 0
-  board[0][1]  = board[1][3];
-  board[1][1]  = board[2][3];
-  board[2][1]  = board[3][3];
-  // right border for row 4
-  board[4][1]  = board[5][3];
-  board[5][1]  = board[6][3];
-  board[6][1]  = board[7][3];
-  // right border for row 8;
-  board[8][1]  = board[9][3];
-  board[9][1]  = board[10][3];
-  board[10][1] = board[11][3];
-  // right border for row 12
-  board[12][1] = board[13][3];
-  board[13][1] = board[14][3];
-  board[14][1] = board[15][3];
-  // bottom border for row 0
-  board[0][2]  = board[4][0];
-  board[1][2]  = board[5][0];
-  board[2][2]  = board[6][0];
-  board[3][3]  = board[7][0];
-  // bottom border for row 4
-  board[4][2]  = board[8][0];
-  board[5][2]  = board[9][0];
-  board[6][2]  = board[10][0];
-  board[7][2]  = board[11][0];
-  // bottom border for row 8
-  board[8][2]  = board[12][0];
-  board[9][2]  = board[13][0];
-  board[10][2] = board[14][0];
-  board[11][2] = board[15][0];
+  // board[0][1]  = board[1][3];
+  // board[1][1]  = board[2][3];
+  // board[2][1]  = board[3][3];
+  // // right border for row 4
+  // board[4][1]  = board[5][3];
+  // board[5][1]  = board[6][3];
+  // board[6][1]  = board[7][3];
+  // // right border for row 8;
+  // board[8][1]  = board[9][3];
+  // board[9][1]  = board[10][3];
+  // board[10][1] = board[11][3];
+  // // right border for row 12
+  // board[12][1] = board[13][3];
+  // board[13][1] = board[14][3];
+  // board[14][1] = board[15][3];
+  // // bottom border for row 0
+  // board[0][2]  = board[4][0];
+  // board[1][2]  = board[5][0];
+  // board[2][2]  = board[6][0];
+  // board[3][2]  = board[7][0];
+  // // bottom border for row 4
+  // board[4][2]  = board[8][0];
+  // board[5][2]  = board[9][0];
+  // board[6][2]  = board[10][0];
+  // board[7][2]  = board[11][0];
+  // // bottom border for row 8
+  // board[8][2]  = board[12][0];
+  // board[9][2]  = board[13][0];
+  // board[10][2] = board[14][0];
+  // board[11][2] = board[15][0];
+}
+
+// Make a square "taken"
+
+function taken() {
+  for (var i = 0; i < board.length; i++) {
+    if (!board[i].includes(0)) {
+      board[i] = player;
+    }
+  }
 }
 /* BEHAVIOR */
 
@@ -74,6 +103,7 @@ function play(y, x) {
   }
   }
   makeEqual();
+  taken();
   console.log(player);
   return board;
 }
